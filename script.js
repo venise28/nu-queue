98f90a7d9c37be7f568090c73ef1414b880cdfd1
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2023-09-28 16:11:01 +0800

    Generate queue working

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function registerStudent() {
+    var studentId = document.getElementById("studentId").value;
+    var office = document.getElementById("modalTitle1").innerText;
+
+    $.ajax({
+        type: "POST",
+        url: "process.php",
+        data: { studentId: studentId, office: office },
+        dataType: "json",
+        success: function (response) {
+            if (response.success) {
+                var queueNumber = response.queue_number;
+                // Set the queue number in the modal
+                document.getElementById("queueNumber").innerText = queueNumber;
+                // Show the third modal
+                $('#thirdModal').modal('show');
+            } else {
+                alert("Error: " + response.message);
+            }
+        },
+        error: function () {
+            alert("An error occurred.");
+        }
+    });
+}
+
+// Function to update the modal titles
+function updateModalTitle(modalId, title) {
+    $(modalId).find(".modal-title").text(title);
+}
+
+// Event listener for button clicks
+$(".btn").click(function() {
+    var modalTitle = $(this).data("title");
+
+    // Update the titles of all three modals
+    updateModalTitle("#firstModal", modalTitle);
+    updateModalTitle("#secondModal", modalTitle);
+    updateModalTitle("#thirdModal", modalTitle);
+});
+
 $(document).ready(function () {
     // Function to update queue numbers on the page
     function updateQueueNumbers() {
@@ -26,6 +67,22 @@ $(document).ready(function () {
     updateQueueNumbers();
     setInterval(updateQueueNumbers, 5000); // Update every 5 seconds
 });
+
+// Function to update queue numbers
+function updateQueueNumbers() {
+    for (const office in queueData) {
+        const queueNumberElement = document.getElementById(`${office}Queue`);
+        if (queueNumberElement) {
+            queueNumberElement.textContent = queueData[office];
+        }
+    }
+}
+
+// Simulate updating queue numbers every 5 seconds
+setInterval(updateQueueNumbers, 5000);
+
+// Initial update
+updateQueueNumbers();
 
 // For DATE AND TIME
 function updateTime() {
